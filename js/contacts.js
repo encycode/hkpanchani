421b8ec0ed5271e006dd9a4328d58d144854f07b
Darshan Vadodariya <darshanvadodariya71@gmail.com>
2022-01-25 23:20:17 +0530

    logoslider js bug fix :)

--- js/contacts.js
+++ js/contacts.js
@@ -97,4 +97,15 @@ var options = {
    density: 'high'
 };
 new ParticleNetwork(canvasDiv, options);
+
 }
+function logoslider() {
+   const root = document.documentElement;
+   const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+   const marqueeContent = document.querySelector("ul.marquee-content");
+   root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+   for (let i = 0; i < marqueeElementsDisplayed; i++) {
+      marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+   }
+}
+
